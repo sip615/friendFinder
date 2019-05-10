6ef7903593d2dafa8bdf5cc8d975ff8becdc498b
sip615 <sunilpatel.328@gmail.com>
2019-05-10 19:38:27 -0400

    another pass through

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+//Possible friends that people can match with.
+let friends = [
+	{
+		name: "",
+		photo: "",
+		scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+	}
+];
+
+//Export the array
+module.exports = friends;
